df1a8729d6545124fa49e774b6eee7d47e9080a3
Loukas Merkouris <loukasmerkouris@gmail.com>
2025-03-25 12:16:39 +0200

    Add django project

---
+++ project_presenter/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("projects.urls")),
+    path("auto-swagger-v1/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "auto-swagger",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
